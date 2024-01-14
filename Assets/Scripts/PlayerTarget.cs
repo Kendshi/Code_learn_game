bf245e8dfbe112157409e0c09db67cb186d2717f
Антон Гамаюнов <anton_gamayunov@mail.ru>
2024-01-14 23:48:02 +1100

    Add TextMesh Pro
    Add scene for level 1

---
+++ Assets/Scripts/PlayerTarget.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using MG_BlocksEngine2.Environment;
+using UnityEngine;
+
+
+public class PlayerTarget : BE2_TargetObject
+{
+    public new Transform Transform => transform;
+}
